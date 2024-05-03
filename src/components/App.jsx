4057f3d13df7b345d3db397a9e6b8c563ec41f02
jn3107 <andrey_gorb@ukr.net>
2024-05-04 00:54:05 +0300

    add html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <a href="">
+          <img src="../images/logo-icon.svg" alt="Logo icon" />
+        </a>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Catalog</a>
+          </li>
+          <li>
+            <a href="">Favorites</a>
+          </li>
+        </ul>
+      </nav>
+      <h1>Camper Rent</h1>
+      <p>
+        We will help you make your travels more interesting and exciting. Our
+        service includes the rental of campers for any style of trip for any
+        company. We are chosen by both regular customers and those who like to
+        try something new. Hurry up, click the button below, and choose an
+        unforgettable experience.
+      </p>
+      <button type="button">Click me!</button>
     </div>
   );
 };
