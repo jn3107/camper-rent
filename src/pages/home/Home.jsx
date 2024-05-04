7cb8a80f44f089ac1da9d9746902e5a3e6b86409
jn3107 <andrey_gorb@ukr.net>
2024-05-04 18:36:29 +0300

    add html Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,11 +12,11 @@ const Home = () => {
     <div>
       <h1>Camper Rent</h1>
       <p>
-        We will help you make your travels more interesting and exciting. Our
-        service includes the rental of campers for any style of trip for any
-        company. We are chosen by both regular customers and those who like to
-        try something new. Hurry up, click the button below, and choose an
-        unforgettable experience
+        We will help you make your travels interesting and exciting. Our
+        services include camper van rentals for any style of trip for any
+        company. We are chosen by regular customers and those who like to try
+        something new. Hurry up, click the button below, and choose an
+        unforgettable experience.
       </p>
       <button type="button" onClick={handlerNavigate}>
         Click me!
